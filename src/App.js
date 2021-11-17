e3afd14cb30ed87aa53200931fbd7c5db5875f43
mohammed-elattar <mseel3ttar@gmail.com>
2021-11-17 23:21:56 +0100

    add json server & materialize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
-
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    M.AutoInit();
+  });
+  return <div className='App'></div>;
 }
 
 export default App;
