95dd0829591b3990ddc419defe6a198aef4fcf4e
mohammed-elattar <mseel3ttar@gmail.com>
2021-11-18 01:16:18 +0100

    EditLogModal & Technician modal components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 function App() {
   useEffect(() => {
     M.AutoInit();
@@ -16,6 +19,9 @@ function App() {
       <div className='container'>
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
